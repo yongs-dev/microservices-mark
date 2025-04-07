d059c40bf7f246134a2a6bd53d421ea4594a151d
Ethan <yongseok993@gmail.com>
2025-04-07 15:57:48 +0900

    config-server containerization

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     id("org.springframework.boot") version "3.1.2"
@@ -11,10 +11,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-aop")
 }
 
-tasks.getByName<BootJar>("bootJar") {
-    enabled = false
-}
-
-tasks.getByName<Jar>("jar") {
-    enabled = true
+tasks.named<BootBuildImage>("bootBuildImage") {
+    imageName.set("${project.group}/config.server:${project.version}")
+    builder.set("paketobuildpacks/builder-jammy-base:latest")
+    environment.set(mapOf("BP_JVM_VERSION" to "21.*"))
 }
---
+++ config-server/src/main/java/com/microservices/mark/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.microservices.mark.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain webSecurityCustomizer(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/actuator/**"),
+                                new AntPathRequestMatcher("/encrypt/**"),
+                                new AntPathRequestMatcher("/decrypt/**"))
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
