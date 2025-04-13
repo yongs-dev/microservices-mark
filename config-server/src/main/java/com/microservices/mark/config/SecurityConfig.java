cd61de0ab8fa9e6c35f8cc799c8320a9e72d4363
Ethan <yongseok993@gmail.com>
2025-04-13 16:28:27 +0900

    property password encoded JASYPT -> JCE

--- config-server/src/main/java/com/microservices/mark/config/SecurityConfig.java
+++ config-server/src/main/java/com/microservices/mark/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -12,7 +13,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain webSecurityCustomizer(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
                         .requestMatchers(new AntPathRequestMatcher("/actuator/**"),
                                 new AntPathRequestMatcher("/encrypt/**"),
                                 new AntPathRequestMatcher("/decrypt/**"))
